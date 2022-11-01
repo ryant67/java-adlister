e15f7dba9eba8fd60c2ed79dda68de333ea51ce8
Ryan Harris <ryan.t.harris67@gmail.com>
2022-11-01 12:07:51 -0500

    servlets exercise complete

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//Annotation informs the compiler what URL Patter to listen for when listening for requests.
+@WebServlet(name = "TestServlet", urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+
+    //Override annotation which is overriding the default implementation from the HttpServlet class.
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        String greeting = request.getParameter("greeting");
+        String person = request.getParameter("person");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + greeting + " from " + person + "!</h1>");
+    }
+}
