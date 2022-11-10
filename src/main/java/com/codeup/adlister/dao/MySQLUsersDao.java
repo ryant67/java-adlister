00cdae2dee3be9a819fc5592c8e153c0073bd047
Ryan Harris <ryan.t.harris67@gmail.com>
2022-11-10 12:01:24 -0600

    exercise started

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
 
@@ -38,10 +39,11 @@ public class MySQLUsersDao implements Users {
     public Long insert(User user) {
         String query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
         try {
+            String hash = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
             PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());
-            stmt.setString(3, user.getPassword());
+            stmt.setString(3, hash);
             stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
